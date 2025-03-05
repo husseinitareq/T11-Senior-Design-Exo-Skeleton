8012bdb67854d4cfaeb1ad7acd227f9521f7c135
Tareq <thussei3@cougarnet.uh.edu>
2025-03-05 11:26:58 -0600

    Added a comment to main.c

--- hello_world_am263x-lp_r5fss0-0_freertos_ti-arm-clang/main.c
+++ hello_world_am263x-lp_r5fss0-0_freertos_ti-arm-clang/main.c
@@ -29,7 +29,7 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
+// Test second commit
 #include <stdlib.h>
 #include <kernel/dpl/DebugP.h>
 #include "ti_drivers_config.h"
